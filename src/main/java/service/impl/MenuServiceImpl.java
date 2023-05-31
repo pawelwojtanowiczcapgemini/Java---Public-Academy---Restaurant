2c62bd831e173cff72ad54fd5563ec8dc912f2ea
pwojtano <pawel.wojtanowicz@capgemini.com>
2023-05-31 14:10:51 +0200

    task 1 - MenuService implemented in MenuServiceImpl, all methods implemented

--- src/main/java/service/impl/MenuServiceImpl.java
+++ src/main/java/service/impl/MenuServiceImpl.java
@@ -1,6 +1,46 @@
 package service.impl;
 
-//TODO IMPLEMENT
-public class MenuServiceImpl {
+import domain.DietType;
+import domain.eto.Meal;
+import domain.eto.Produce;
+import service.api.MenuService;
 
+import java.util.List;
+
+//TODO IMPLEMENT
+public class MenuServiceImpl implements MenuService {
+    @Override
+    public List<Meal> findVegetarianFood(List<Meal> meals) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodByType(List<Meal> meals, DietType diet) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodCheaperThanPrice(List<Meal> meals, Integer price) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodCheaperWithCalories(List<Meal> meals, Integer minCalories, Integer maxCalories) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodCheaperThanPrice(List<Meal> meals, String name) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodContaining(List<Meal> meals, Produce product) {
+        return null;
+    }
+
+    @Override
+    public List<Meal> findFoodExcludingAll(List<Meal> meals, List<Produce> products) {
+        return null;
+    }
 }
